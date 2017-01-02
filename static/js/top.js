2affa49b708e2899a661e7b382df1ff29c83a504
XIZERO <f12998765@yeah.net>
2017-01-02 13:20:21 +0800

    折腾主题
    新的样式，新的标签云，简介纯粹

--- static/js/top.js
+++ static/js/top.js
@@ -8,6 +8,7 @@ function goTop()
             $("#gotop").fadeOut(500);//以1秒的间隔渐隐id=gotop的元素
     });
 };
+
 $(function(){
     //点击回到顶部的元素
     $("#gotop").click(function(e) {
@@ -15,11 +16,11 @@ $(function(){
             $('body,html').animate({scrollTop:0},500);
     });
     $("#gotop").mouseover(function(e) {
-        $(this).css("background","url(../img/z.png) no-repeat");
+        $(this).css("background","url({{ .Site.BaseURL }}/img/z.png) no-repeat");
         $(this).css("background-size","100% 100%");
     });
     $("#gotop").mouseout(function(e) {
-        $(this).css("background","url(../img/z.png) no-repeat");
+        $(this).css("background","url({{ .Site.BaseURL }}/img/z.png) no-repeat");
         $(this).css("background-size","100% 100%");
     });
     goTop();//实现回到顶部元素的渐显与渐隐
